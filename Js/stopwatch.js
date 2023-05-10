8f3e29fce0f07dc38951813895e1156ef8a46aa1
Ahmad <ali.ah.naeem2704@gmail.com>
2023-05-10 05:39:49 -0400

    ### stopwatch object

---
+++ Js/stopwatch.js
@@ -0,0 +1,52 @@
+
+
+function Stopwatch(value){
+    let startTime, endTime, running, duration = 0;
+
+
+    let startFunction = function(){
+
+        if(running){
+            throw new Error('Stopwatch is already started')
+        }
+        running = true;
+        startTime = new Date();
+    }
+    let resetFunction = function(){
+        startTime = null,
+        endTime = null,
+        running = false, 
+        duration = 0;
+    }
+    let stopFunction = function(){
+        if(!running){
+            throw new Error('Stopwatch is already started')
+        }
+        running = false;
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds
+    }
+    
+
+    this.start =  startFunction
+    this.reset =  resetFunction
+    this.stop =  stopFunction
+
+    Object.defineProperty(this,'duration',{
+        get:function(){
+            return duration
+        },
+    })
+    
+}
+
+const sw = new Stopwatch(10)
+console.log(sw)
+
+// run these commands
+// sw.start()     to start the stopwatch
+// sw.stop()     to stop the stopwatch
+// sw.reset()     to reset the stopwatch
+// sw.duration()     to get total time duration of the stopwatch
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # object-oriented-programming-learning
 learn object oriented programming
+
+In Js folder the Stopwatch file will give you information about how the javascript object works
