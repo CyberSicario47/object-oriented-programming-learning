61d84e07ba120977cc38780df6c0304f5cbeded1
Ahmad <ali.ah.naeem2704@gmail.com>
2023-07-24 10:34:12 -0400

    ### done

--- formApp/formik-form-hook/src/Components/Form/From.jsx
+++ formApp/formik-form-hook/src/Components/Form/From.jsx
@@ -7,11 +7,14 @@ const From = () => {
     lastName: yup.string().required(),
     email: yup.string().email().required(),
     age: yup.string().required(),
-    password: yup.string().min(5).max(8).required(),
-    confirmPassword: yup.string().oneOf([yup.ref("passowrd"), null]),
+    password: yup.string().min(5).max(10).required(),
+    confirmPassword: yup
+    .string()
+    .required("Please re-type your password")
+    .oneOf([yup.ref("passowrd"), null],'The password must match!!'),
   });
 
-  const { register, handleSubmit, errors } = useForm({
+  const { register, handleSubmit, formState:{errors} } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -28,45 +31,49 @@ const From = () => {
               type="text"
               name="firstName"
               placeholder="First Name ..."
-            //   ref={register}
+              {...register("firstName")}
             />
-            {/* <p>{errors?.firstName?.message}</p> */}
-            {/* <input
+            <p>{errors?.firstName?.message}</p>
+            <input
               type="text"
               name="lastName"
               placeholder="Lasr Name ..."
-              ref={register}
+              {...register("lastName")}
             />
-            <p>{errors?.lastName?.message}</p> */}
-            {/* <input
+            <p>{errors?.lastName?.message}</p>
+            <input
               type="email"
               name="email"
               placeholder="Email ..."
-              ref={register}
+              {...register("email")}
             />
-            <p>{errors?.email?.message}</p> */}
-            {/* <input
+            <p>{errors?.email?.message}</p>
+            <input
               type="text"
               name="age"
               placeholder="Age ..."
-              ref={register}
+              {...register("age")}
             />
-            <p>{errors?.age?.message}</p> */}
-            {/* <input
+            <p>{errors?.age?.message}</p>
+            <input
               type="password"
               name="password"
               placeholder="Password ..."
-              ref={register}
+              {...register("password")}
             />
-            <p>{errors?.password?.message}</p> */}
-            {/* <input
+            <p>{errors?.password?.message}</p>
+            <input
               type="password"
               name="confirmPassword"
               placeholder="Confirm Password ..."
-              ref={register}
+              {...register('confirmPassword')} 
             />
-            <p>{errors?.password && "Passwords must match"}</p> */}
-            {/* <input type="submit" id="submit" ref={register} /> */}
+            <p>{errors?.confirmPassword?.message}</p>
+            <input
+              type="submit"
+              id="submit"
+              // ref={register}
+            />
           </form>
         </div>
       </div>
