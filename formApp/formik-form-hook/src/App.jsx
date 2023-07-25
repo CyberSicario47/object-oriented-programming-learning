13e4f4221b2fb25de36ba95f3eb8a04f37f04b9d
Ahmad <ali.ah.naeem2704@gmail.com>
2023-07-25 07:50:41 -0400

    ### tailwind css

--- formApp/formik-form-hook/src/App.jsx
+++ formApp/formik-form-hook/src/App.jsx
@@ -4,7 +4,273 @@ function App() {
   return (
     <>
       <ReactQuery />
+      <div>
+        <h1 className="bg-blue-500 shadow-lg blue-05 font-mono space-y-9">
+          THis is the Tailwind
+        </h1>
+        <div className="space-y-10">
+          <div className="w-96 bg-white shadow rounded">w-96</div>
+          <div className="w-80 bg-white shadow rounded">w-80</div>
+          <div className="w-72 bg-white shadow rounded">w-72</div>
+          <div className="w-64 bg-white shadow rounded">w-64</div>
+          <div className="w-60 bg-white shadow rounded">w-60</div>
+          <div className="w-56 bg-white shadow rounded">w-56</div>
+          <div className="w-52 bg-white shadow rounded">w-52</div>
+          <div className="w-48 bg-white shadow rounded">w-48</div>
+        </div>
+        <div className="grid grid-cols-4 gap-2">
+          <div className="bg-sky-50 aspect-square"></div>
+          <div className="bg-sky-100 aspect-square"></div>
+          <div className="bg-sky-200 aspect-square"></div>
+          <div className="bg-sky-300 aspect-square"></div>
+          <div className="bg-sky-400 aspect-square"></div>
+          <div className="bg-sky-500 aspect-square"></div>
+          <div className="bg-sky-600 aspect-square"></div>
+          <div className="bg-sky-700 aspect-square"></div>
+          <div className="bg-sky-800 aspect-square"></div>
+          <div className="bg-sky-900 aspect-square"></div>
+        </div>
 
+        <div className="flex font-sans space-y-10 rounded bg-violet-100 shadow mt-5">
+          <div className="flex-none w-48 relative">
+            <img
+              src="https://tailwindcss.com/_next/static/media/classic-utility-jacket.82031370.jpg"
+              alt=""
+              className="absolute inset-0 w-full h-full object-cover"
+              loading="lazy"
+            />
+          </div>
+          <form className="flex-auto p-6">
+            <div className="flex flex-wrap">
+              <h1 className="flex-auto text-lg font-semibold text-slate-900">
+                Classic Utility Jacket
+              </h1>
+              <div className="text-lg font-semibold text-slate-500">
+                $110.00
+              </div>
+              <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+                In stock
+              </div>
+            </div>
+            <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+              <div className="space-x-2 flex text-sm">
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="xs"
+                    checked
+                  />
+                  <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                    XS
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="s"
+                  />
+                  <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                    S
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="m"
+                  />
+                  <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                    M
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="l"
+                  />
+                  <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                    L
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="xl"
+                  />
+                  <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                    XL
+                  </div>
+                </label>
+              </div>
+            </div>
+            <div className="flex space-x-4 mb-6 text-sm font-medium">
+              <div className="flex-auto flex space-x-4">
+                <button
+                  className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+                  type="submit"
+                >
+                  Buy now
+                </button>
+                <button
+                  className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900"
+                  type="button"
+                >
+                  Add to bag
+                </button>
+              </div>
+              <button
+                className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
+                type="button"
+                aria-label="Like"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <p className="text-sm text-slate-700">
+              Free shipping on all continental US orders.
+            </p>
+          </form>
+        </div>
+
+        <div className="flex p-6 font-mono mt-10">
+          <div className="flex-none w-48 mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-teal-400">
+            <img
+              src="https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg"
+              alt=""
+              className="absolute z-10 inset-0 w-full h-full object-cover rounded-lg"
+              loading="lazy"
+            />
+          </div>
+          <form className="flex-auto pl-6">
+            <div className="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6">
+              <h1 className="relative w-full flex-none mb-2 text-2xl font-semibold text-white">
+                Retro Shoe
+              </h1>
+              <div className="relative text-lg text-white">$89.00</div>
+              <div className="relative uppercase text-teal-400 ml-3">
+                In stock
+              </div>
+            </div>
+            <div className="flex items-baseline my-6">
+              <div className="space-x-3 flex text-sm font-medium">
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="xs"
+                    checked
+                  />
+                  <div className="relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">
+                    XS
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="s"
+                  />
+                  <div className="relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">
+                    S
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="m"
+                  />
+                  <div className="relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">
+                    M
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="l"
+                  />
+                  <div className="relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">
+                    L
+                  </div>
+                </label>
+                <label>
+                  <input
+                    className="sr-only peer"
+                    name="size"
+                    type="radio"
+                    value="xl"
+                  />
+                  <div className="relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">
+                    XL
+                  </div>
+                </label>
+              </div>
+            </div>
+            <div className="flex space-x-2 mb-4 text-sm font-medium">
+              <div className="flex space-x-4">
+                <button
+                  className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-black"
+                  type="submit"
+                >
+                  Buy now
+                </button>
+                <button
+                  className="px-6 h-12 uppercase font-semibold tracking-wider border border-slate-200 text-slate-900"
+                  type="button"
+                >
+                  Add to bag
+                </button>
+              </div>
+              <button
+                className="flex-none flex items-center justify-center w-12 h-12 text-black"
+                type="button"
+                aria-label="Like"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <p className="text-xs leading-6 text-slate-500">
+              Free shipping on all continental US orders.
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
--- formApp/formik-form-hook/src/main.jsx
+++ formApp/formik-form-hook/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClientProvider, QueryClient } from "react-query";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
